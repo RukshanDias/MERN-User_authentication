9ca7dcb1009aa605d5bc6b703582645c8aff3c53
RukshanDias <rukshandias002@gmail.com>
2023-06-27 09:35:32 +0530

    created model

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        quote: {
+            type: String,
+        },
+    },
+    { collection: "user-data" }
+);
+
+const model = mongoose.model("Userdata", User);
+
+module.exports = model;
