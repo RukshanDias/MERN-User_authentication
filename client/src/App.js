cc20406f0344b6642dd234dd794709604727244c
RukshanDias <rukshandias002@gmail.com>
2023-06-27 12:49:19 +0530

    created on react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,21 @@
-import "./App.css";
 import React from "react";
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    async function registerUser(event) {
-        event.preventDefault();
-        const response = await fetch("http://localhost:8000/api/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name,
-                email,
-                password,
-            }),
-        });
-        const data = await response.json();
-    }
+const App = () => {
     return (
-        <div className="App">
-            <h1>Register</h1>
-            <form onSubmit={registerUser}>
-                <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Name" />
-                <br />
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
-                <br />
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
-                <br />
-                <input type="submit" value="Register" />
-            </form>
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" Component={Login} />
+                    <Route path="/register" Component={Register} />
+                    <Route path="/dashboard" Component={Dashboard} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
-}
+};
 
 export default App;
