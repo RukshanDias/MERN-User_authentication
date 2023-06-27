2b8bfa8296e11e5be569e878e1cb9afe55d8dd81
RukshanDias <rukshandias002@gmail.com>
2023-06-27 09:10:13 +0530

    make req & res as JSON

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors()); // cors middleware: use casue have differnt server paths in client & server
+app.use(express.json()); // pass anything in req as json
 
-app.get("/hello", (req, res) => {
-    res.send("hello world");
+app.post("/api/register", async (req, res) => {
+    console.log(req.body);
+    res.send({ status: "ok" });
 });
 
 app.listen(8000, () => {
