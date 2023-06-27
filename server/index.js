c7f1136bc82908c0c0581aba57d43714ec360059
RukshanDias <rukshandias002@gmail.com>
2023-06-27 08:41:30 +0530

    created initial server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// import
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(8000, () => {
+    console.log("server started");
+});
