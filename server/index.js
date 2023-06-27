252e78b656b5bf3f941e4ca6c8752c81ef50b71f
RukshanDias <rukshandias002@gmail.com>
2023-06-27 12:49:06 +0530

    jwt, decode, quote display on login

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
 
 app.use(cors()); // cors middleware: use casue have differnt server paths in client & server
 app.use(express.json()); // pass anything in req as json
@@ -21,6 +22,52 @@ app.post("/api/register", async (req, res) => {
         res.json({ status: "ok" });
     } catch (err) {
         res.json({ status: "error", error: "Duplicate email" });
+    }
+});
+
+// Login
+app.post("/api/login", async (req, res) => {
+    console.log(req.body);
+    const user = await User.findOne({ email: req.body.email, password: req.body.password }); // bad to store pass like this -> password: req.body.password
+    if (user) {
+        const token = jwt.sign(
+            {
+                name: user.name,
+                email: user.email,
+            },
+            process.env.JWT_SECRET_KEY
+        );
+        res.json({ status: "ok", user: token });
+    } else {
+        res.json({ status: "error", error: "email password dont match", user: false });
+    }
+});
+
+// qoute
+app.get("/api/quote", async (req, res) => {
+    const token = req.headers["x-access-token"];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const email = decoded.email;
+        const user = await User.findOne({ email: email });
+        return res.json({ status: "ok", quote: user.quote });
+    } catch (error) {
+        console.log(error);
+        res.json({ status: "error", error: "invalid token" });
+    }
+});
+
+// create qoute
+app.post("/api/quote", async (req, res) => {
+    const token = req.headers["x-access-token"];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const email = decoded.email;
+        await User.updateOne({ email: email }, { $set: { quote: req.body.quote } });
+        return res.json({ status: "ok" });
+    } catch (error) {
+        console.log(error);
+        res.json({ status: "error", error: "invalid token" });
     }
 });
 
